3bd9e7d3c890401ee3cc0c2bc65aa21877cb2e7f
Mo Di <ddddmo@outlook.com>
2024-12-17 15:24:18 +0800

    tidy

--- test.sh
+++ test.sh
@@ -4,15 +4,15 @@ docker run --rm -i \
     -u $(id -u):$(id -g) \
     -v $(pwd):/work -w /work \
     modidh/pandoc \
-	--pdf-engine=xelatex \
-	--toc \
-	--toc-depth 2 \
-	-V toc-title=目录 \
-	-V pagestyle:plain \
-	-V documentclass=ctexart \
-	-V pagesize=a4 \
-	-V geometry:margin=1in \
-	-V indent:true \
-	-V linkcolor:blue \
-	-o output.pdf \
-	examples/input.md
+    --pdf-engine=xelatex \
+    --toc \
+    --toc-depth 2 \
+    -V toc-title=目录 \
+    -V pagestyle:plain \
+    -V documentclass=ctexart \
+    -V pagesize=a4 \
+    -V geometry:margin=1in \
+    -V indent:true \
+    -V linkcolor:blue \
+    -o output.pdf \
+    examples/input.md
